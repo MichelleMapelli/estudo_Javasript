b92bba2c9d62b38cb5a8ef56a75f2c172f41e323
MichelleMapelli <mi.mapelli@gmail.com>
2019-01-23 10:35:14 -0200

    adicionando variaveis para ter retorno do imc

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,13 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Endrocnologista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var tdAltura = document.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+console.log(imc);
