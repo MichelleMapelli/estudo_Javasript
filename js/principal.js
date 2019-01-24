d9f381de631a75c73cf0e008d0994a338ac4ef3b
MichelleMapelli <mi.mapelli@gmail.com>
2019-01-24 09:55:39 -0200

    organizando o código

--- js/principal.js
+++ js/principal.js
@@ -6,27 +6,25 @@ var pacientes = document.querySelectorAll(".paciente");
 for (var i = 0; i < pacientes.length; i++) {
     //Colocando o array dentro de uma variável
     var paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+    var tdPeso = paciente.querySelector(".info-peso").textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura").textContent;
 
     var tdImc = paciente.querySelector(".info-imc");
 
     var pesoEhValido = true;
     var alturaEhValida = true;
 
-    if (peso <= 0 || peso >= 500) {
+    if (tdPeso <= 0 || tdPeso >= 500) {
         pesoEhValido = false;
         tdImc.textContent = "Peso é inválido!";
     }
-    if (altura <= 0 || altura >= 3) {
+    if (tdAltura <= 0 || tdAltura >= 3) {
         alturaEhValida = false;
         tdImc.textContent = "Altura é inválida!";
     }
     if (pesoEhValido && alturaEhValida) {
-        var imc = peso / (altura * altura);
+        var imc = tdPeso / (tdAltura * tdAltura);
         tdImc.textContent = imc.toFixed(2);
     }
 
